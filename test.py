b5b7250b2e40804f142531a1c05812cf0d294e77
jjzqp <jjzqpx@163.com>
2019-12-05 17:48:22 +0800

    上传文档

---
+++ test.py
@@ -0,0 +1,14 @@
+
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("my calc")
+window.geometry("640x480")
+e=tk.Text(window,width=640,height=480)
+e.pack()
+        
+
+
+
+window.mainloop()
